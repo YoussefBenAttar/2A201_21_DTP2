002a924b14bd01723524567b1812f8d3312329f1
YoussefBenAttar <93948388+YoussefBenAttar@users.noreply.github.com>
2021-12-04 00:04:11 +0100

    Add files via upload

---
+++ 2A201_21_DTP2.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace _2A_201_CTP1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(390, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 19);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "new";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(390, 70);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 19);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "research";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(390, 99);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 22);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Add";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(390, 131);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(88, 25);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Modification";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(390, 166);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(88, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "delete";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(390, 195);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(88, 29);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Exit";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(513, 288);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ 2A201_21_DTP2.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace _2A_201_CTP1
+{
+    public partial class Form1 : Form
+    {
+        private Database db;
+
+        public Form1()
+        {
+            InitializeComponent()
+        private void Form1_Load(object sender, EventArgs e db = new Database()
+            db.Connecter()
+            RefreshDataGrid();
+        
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            db.Deconnecter()
+        private void btnAjouter_Click(object sender, EventArgs e)
+                { 
+           
+            db.cmd.CommandText = "INSERT INTO Etudiant values(@cin, @nom, @prenom, @age)";
+
+            db.cmd.Connection = db.connection;
+
+            db.cmd.Parameters.Clear();
+
+
+                    if (numID.Value != 0 && txtNom.Text != "" && txtPrenom.Text != "" && numAge.Value > 18)
+                    {
+                        db.cmd.Parameters.AddWithValue("@cin", numID.Value);
+                        db.cmd.Parameters.AddWithValue("@nom", txtNom.Text);
+                        db.cmd.Parameters.AddWithValue("@prenom", txtPrenom.Text);
+                        db.cmd.Parameters.AddWithValue("@age", numAge.Value);
+
+                        db.cmd.ExecuteNonQuery();
+
+
+                        RefreshDataGrid();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Merci de remplir tous les champs");
+                    }
+
+
+        }
+
+        private void btnModifier_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSupprimer_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFermer_Click(object sender, EventArgs e)
+        {
+            db.Deconnecter();
+            this.Close();
+        }
+
+
+
+       
+        public void RefreshDataGrid()
+        {
+            db.dataReader = db.GetAll();
+
+            DataTable dataTable = new DataTable();
+
+           
+            dataTable.Load(db.dataReader);
+
+            
+            dataGridView.DataSource = dataTable;
+        }
+    }
+}
+
